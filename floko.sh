d787d736b937232099d410752b76c1c17cf4fef9
lindwurm <lindwurm.q@gmail.com>
2023-02-10 05:38:58 +0900

    floko: fix wrong remove to untracked files

--- floko.sh
+++ floko.sh
@@ -6,22 +6,18 @@ build_home="${HOME}/android/floko"
 cd ${build_home}/bootable/recovery
 echo ">> [$(date)] Applying the patch to bootable/recovery"
 git apply -p1 "${path_to_patches}/bootable-recovery.diff"
-git clean -fd
 
 cd ${build_home}/build/make
 echo ">> [$(date)] Applying the patch to build/make"
 patch --quiet --force -p1 -i "${path_to_patches}/build-make.patch"
-git clean -fd
 
 cd ${build_home}/build/soong
 echo ">> [$(date)] Applying the patch to build/soong"
 patch --quiet --force -p1 -i "${path_to_patches}/build-soong.patch"
-git clean -fd
 
 cd ${build_home}/frameworks/base
 echo ">> [$(date)] Applying the patch to frameworks/base"
 patch --quiet --force -p1 -i "${path_to_patches}/frameworks-base.patch"
-git clean -fd
 # This is workaround; If the patch failed because something changed in upstream, remove this file anyway.
 git rm core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
 
@@ -29,36 +25,29 @@ cd ${build_home}/lineage-sdk
 echo ">> [$(date)] Applying the patch to lineage-sdk"
 patch --quiet --force -p1 -i "${path_to_patches}/lineage-sdk.patch"
 find lineage/res/res/values*/strings.xml -type f | xargs sed -i -e "s/crDroid/FlokoROM/g"
-git clean -fd
 
 cd ${build_home}/packages/apps/crDroidSettings
 echo ">> [$(date)] Applying the patch to packages/apps/crDroidSettings"
 patch --quiet --force -p1 -i "${path_to_patches}/packages-apps-crDroidSettings.patch"
-git clean -fd
 
 cd ${build_home}/packages/apps/LineageParts
 echo ">> [$(date)] Applying the patch to packages/apps/LineageParts"
 patch --quiet --force -p1 -i "${path_to_patches}/packages-apps-LineageParts.patch"
 find res/values*/strings.xml -type f | xargs sed -i -e "s/crDroid/FlokoROM/g"
-git clean -fd
 
 cd ${build_home}/packages/apps/Settings
 echo ">> [$(date)] Applying the patch to packages/apps/Settings"
 git apply -p1 "${path_to_patches}/packages-apps-Settings.diff"
-git clean -fd
 
 cd ${build_home}/packages/apps/SetupWizard
 echo ">> [$(date)] Applying the patch to packages/apps/SetupWizard"
 git apply -p1 "${path_to_patches}/packages-apps-SetupWizard.diff"
 find res/values*/strings.xml -type f | xargs sed -i -e "s/crDroid/FlokoROM/g"
-git clean -fd
 
 cd ${build_home}/vendor/addons
 echo ">> [$(date)] Applying the patch to vendor/addons"
 git apply -p1 "${path_to_patches}/vendor-addons.diff"
-git clean -fd
 
 cd ${build_home}/vendor/lineage
 echo ">> [$(date)] Applying the patch to vendor/lineage"
 patch --quiet --force -p1 -i "${path_to_patches}/vendor-lineage.patch"
-git clean -fd
