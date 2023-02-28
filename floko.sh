b1e370ad6f7fbfbe5d19d3a5acb5616a0e65f399
lindwurm <lindwurm.q@gmail.com>
2023-03-01 00:48:51 +0900

    floko: Rename patches
    * Use underscore instead of hyphen
    * Rename build-make to build, use repo name
    * Fix feb update of packages_apps_Settings
    
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- floko.sh
+++ floko.sh
@@ -6,19 +6,19 @@ build_home="${HOME}/android/floko"
 
 cd ${build_home}/bootable/recovery
 echo ">> [$(date)] Applying the patch to bootable/recovery"
-git apply -p1 "${path_to_patches}/bootable-recovery.diff"
+git apply -p1 "${path_to_patches}/bootable_recovery.diff"
 
 cd ${build_home}/build/make
 echo ">> [$(date)] Applying the patch to build/make"
-patch --quiet --force -p1 -i "${path_to_patches}/build-make.patch"
+patch --quiet --force -p1 -i "${path_to_patches}/build.patch"
 
 cd ${build_home}/build/soong
 echo ">> [$(date)] Applying the patch to build/soong"
-patch --quiet --force -p1 -i "${path_to_patches}/build-soong.patch"
+patch --quiet --force -p1 -i "${path_to_patches}/build_soong.patch"
 
 cd ${build_home}/frameworks/base
 echo ">> [$(date)] Applying the patch to frameworks/base"
-patch --quiet --force -p1 -i "${path_to_patches}/frameworks-base.patch"
+patch --quiet --force -p1 -i "${path_to_patches}/frameworks_base.patch"
 # This is workaround; If the patch failed because something changed in upstream, remove this file anyway.
 echo ">> [$(date)] Removing PixelPropsUtils.java"
 git rm core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -30,29 +30,29 @@ find lineage/res/res/values*/strings.xml -type f | xargs sed -i -e "s/crDroid/Fl
 
 cd ${build_home}/packages/apps/crDroidSettings
 echo ">> [$(date)] Applying the patch to packages/apps/crDroidSettings"
-patch --quiet --force -p1 -i "${path_to_patches}/packages-apps-crDroidSettings.patch"
+patch --quiet --force -p1 -i "${path_to_patches}/packages_apps_crDroidSettings.patch"
 
 cd ${build_home}/packages/apps/LineageParts
 echo ">> [$(date)] Applying the patch to packages/apps/LineageParts"
-patch --quiet --force -p1 -i "${path_to_patches}/packages-apps-LineageParts.patch"
+patch --quiet --force -p1 -i "${path_to_patches}/packages_apps_LineageParts.patch"
 find res/values*/strings.xml -type f | xargs sed -i -e "s/crDroid/FlokoROM/g"
 
 cd ${build_home}/packages/apps/Settings
 echo ">> [$(date)] Applying the patch to packages/apps/Settings"
-git apply -p1 "${path_to_patches}/packages-apps-Settings.diff"
+git apply -p1 "${path_to_patches}/packages_apps_Settings.diff"
 
 cd ${build_home}/packages/apps/SetupWizard
 echo ">> [$(date)] Applying the patch to packages/apps/SetupWizard"
-git apply -p1 "${path_to_patches}/packages-apps-SetupWizard.diff"
+git apply -p1 "${path_to_patches}/packages_apps_SetupWizard.diff"
 find res/values*/strings.xml -type f | xargs sed -i -e "s/crDroid/FlokoROM/g"
 
 cd ${build_home}/vendor/addons
 echo ">> [$(date)] Applying the patch to vendor/addons"
-git apply -p1 "${path_to_patches}/vendor-addons.diff"
+git apply -p1 "${path_to_patches}/vendor_addons.diff"
 
 cd ${build_home}/vendor/lineage
 echo ">> [$(date)] Applying the patch to vendor/lineage"
-patch --quiet --force -p1 -i "${path_to_patches}/vendor-lineage.patch"
+patch --quiet --force -p1 -i "${path_to_patches}/vendor_lineage.patch"
 # This is workaround; If the patch failed because something changed in upstream, remove this file anyway.
 echo ">> [$(date)] Removing version.mk"
 git rm config/version.mk
