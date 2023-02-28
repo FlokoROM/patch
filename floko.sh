1e1c5e35843a072b1e3515f313e4e1a6e14ace50
lindwurm <lindwurm.q@gmail.com>
2023-03-01 01:43:42 +0900

    floko: Use `.diff` instead of `.patch`
    * and use only `git apply` to make script more concise
    
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- floko.sh
+++ floko.sh
@@ -1,58 +1,55 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-path_to_patches="$(pwd)"
 build_home="${HOME}/android/floko"
+path_to_patches="$(pwd)"
+
+apply_patch() {
+    echo ">> [$(date)] Applying the patch to $(pwd | sed 's@.*floko/@@g')"
+    git apply -p1 "${path_to_patches}/${1}.diff"
+}
 
 cd ${build_home}/bootable/recovery
-echo ">> [$(date)] Applying the patch to bootable/recovery"
-git apply -p1 "${path_to_patches}/bootable_recovery.diff"
+apply_patch bootable_recovery
 
 cd ${build_home}/build/make
-echo ">> [$(date)] Applying the patch to build/make"
-patch --quiet --force -p1 -i "${path_to_patches}/build.patch"
+apply_patch build
 
 cd ${build_home}/build/soong
-echo ">> [$(date)] Applying the patch to build/soong"
-patch --quiet --force -p1 -i "${path_to_patches}/build_soong.patch"
+apply_patch build_soong
 
 cd ${build_home}/frameworks/base
-echo ">> [$(date)] Applying the patch to frameworks/base"
-patch --quiet --force -p1 -i "${path_to_patches}/frameworks_base.patch"
+apply_patch frameworks_base
 # This is workaround; If the patch failed because something changed in upstream, remove this file anyway.
-echo ">> [$(date)] Removing PixelPropsUtils.java"
+echo ">> [$(date)] -- Removing PixelPropsUtils.java"
 git rm core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
 
 cd ${build_home}/lineage-sdk
-echo ">> [$(date)] Applying the patch to lineage-sdk"
-patch --quiet --force -p1 -i "${path_to_patches}/lineage-sdk.patch"
+apply_patch lineage-sdk
+echo ">> [$(date)] -- Rebranding crDroid to FlokoROM"
 find lineage/res/res/values*/strings.xml -type f | xargs sed -i -e "s/crDroid/FlokoROM/g"
 
 cd ${build_home}/packages/apps/crDroidSettings
-echo ">> [$(date)] Applying the patch to packages/apps/crDroidSettings"
-patch --quiet --force -p1 -i "${path_to_patches}/packages_apps_crDroidSettings.patch"
+apply_patch packages_apps_crDroidSettings
 
 cd ${build_home}/packages/apps/LineageParts
-echo ">> [$(date)] Applying the patch to packages/apps/LineageParts"
-patch --quiet --force -p1 -i "${path_to_patches}/packages_apps_LineageParts.patch"
+apply_patch packages_apps_LineageParts
+echo ">> [$(date)] -- Rebranding crDroid to FlokoROM"
 find res/values*/strings.xml -type f | xargs sed -i -e "s/crDroid/FlokoROM/g"
 
 cd ${build_home}/packages/apps/Settings
-echo ">> [$(date)] Applying the patch to packages/apps/Settings"
-git apply -p1 "${path_to_patches}/packages_apps_Settings.diff"
+apply_patch packages_apps_Settings
 
 cd ${build_home}/packages/apps/SetupWizard
-echo ">> [$(date)] Applying the patch to packages/apps/SetupWizard"
-git apply -p1 "${path_to_patches}/packages_apps_SetupWizard.diff"
+apply_patch packages_apps_SetupWizard
+echo ">> [$(date)] -- Rebranding crDroid to FlokoROM"
 find res/values*/strings.xml -type f | xargs sed -i -e "s/crDroid/FlokoROM/g"
 
 cd ${build_home}/vendor/addons
-echo ">> [$(date)] Applying the patch to vendor/addons"
-git apply -p1 "${path_to_patches}/vendor_addons.diff"
+apply_patch vendor_addons
 
 cd ${build_home}/vendor/lineage
-echo ">> [$(date)] Applying the patch to vendor/lineage"
-patch --quiet --force -p1 -i "${path_to_patches}/vendor_lineage.patch"
+apply_patch vendor_lineage
 # This is workaround; If the patch failed because something changed in upstream, remove this file anyway.
-echo ">> [$(date)] Removing version.mk"
+echo ">> [$(date)] -- Removing version.mk"
 git rm config/version.mk
